893bd09dc18eb5f67c6fe4a5989aa6cc30347351
qswdl <414946742@qq.com>
2017-07-07 15:51:15 +0800

    modify

--- src/main/java/com/yidatec/CrmApplication.java
+++ src/main/java/com/yidatec/CrmApplication.java
@@ -1,9 +1,14 @@
 package com.yidatec;
 
+import com.yidatec.util.ConfProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
+@EnableConfigurationProperties({ConfProperties.class})
 public class CrmApplication {
 
 	public static void main(String[] args) {

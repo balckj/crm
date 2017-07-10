4b238fa4204141a08a9c437e795a6c5bd8d45a92
Hello World <308864211@qq.com>
2017-07-11 07:39:49 +0800

    controller edit

--- src/main/java/com/yidatec/controller/ProjectManagerController.java
+++ src/main/java/com/yidatec/controller/ProjectManagerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by Administrator on 2017/7/10.
  */
 @Controller
-public class projectManagerContrlloer extends BaseController{
+public class ProjectManagerController extends BaseController{
     @RequestMapping("/projectManagerIndex")
     public String projectManagerIndex(){
         return "prejectManager";

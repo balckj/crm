f01c0e1aa62d08e6e6f9aca70236d88a8397b91a
qswdl <414946742@qq.com>
2017-07-25 10:27:35 +0800

    modify

--- src/main/java/com/yidatec/model/User.java
+++ src/main/java/com/yidatec/model/User.java
@@ -27,18 +27,18 @@ import java.util.List;
 public class User extends BaseModel implements UserDetails {
 
 
-    @NotBlank(message = "必须输入姓名", groups = { })
-    @Length(min = 2, max = 40, message = "姓名必须由2到40个字符组成", groups = { })
+    @NotBlank(message = "必须输入姓名", groups = {UserValidatePM.class,UserValidateDesigner.class,UserValidateSale.class,UserValidateSpectator.class ,UserValidate.class})
+    @Length(min = 2, max = 40, message = "姓名必须由2到40个字符组成", groups = {UserValidatePM.class,UserValidateDesigner.class,UserValidateSale.class,UserValidateSpectator.class ,UserValidate.class })
     private String name;
     private String avatar;
     //用户以电话号码标识，如果没有注册电话号码就无法约课，电话号码是必须的
-    @NotBlank(message = "必须输入手机号码", groups = { })
-    @Pattern(regexp="^1[3|4|5|7|8][0-9]\\d{4,8}$",message="手机号码格式不正确", groups = { })
+    @NotBlank(message = "必须输入手机号码", groups = {UserValidatePM.class,UserValidateDesigner.class,UserValidateSale.class,UserValidateSpectator.class ,UserValidate.class })
+    @Pattern(regexp="^1[3|4|5|7|8][0-9]\\d{4,8}$",message="手机号码格式不正确", groups = {UserValidatePM.class,UserValidateDesigner.class,UserValidateSale.class,UserValidateSpectator.class ,UserValidate.class })
     private String mobilePhone;
-    @Email(message="邮箱格式不正确", groups = { })
+    @Email(message="邮箱格式不正确", groups = {UserValidatePM.class,UserValidateDesigner.class,UserValidateSale.class,UserValidateSpectator.class ,UserValidate.class })
     private String email;
     private String openId;
-    @NotBlank(message = "必须输入密码", groups = { })
+    @NotBlank(message = "必须输入密码", groups = {UserValidatePM.class,UserValidateDesigner.class,UserValidateSale.class,UserValidateSpectator.class ,UserValidate.class })
     @Pattern(regexp="(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{5,10}",message="密码必须是5~10位数字和字母的组合", groups = { })
     private String password;
 

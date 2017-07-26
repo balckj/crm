db3219c309d525a65e015ceea1e3f8b8494e0288
dafei <344739106@qq.com>
2017-07-26 11:06:32 +0800

    联系人

---
+++ src/main/java/com/yidatec/model/Contact.java
@@ -0,0 +1,52 @@
+package com.yidatec.model;
+
+/**
+ * Created by Administrator on 2017/7/25.
+ */
+public class Contact extends BaseModel{
+    private String name;
+    private String mobilePhone;
+    private String position;
+    private String email;
+    private String state;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}

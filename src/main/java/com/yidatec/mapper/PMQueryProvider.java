0ab559325ac2645231d0b114e87578e3ea41abb3
HELLO WORLD <308864211@qq.com>
2017-07-31 15:56:53 +0800

    PM update

--- src/main/java/com/yidatec/mapper/PMQueryProvider.java
+++ src/main/java/com/yidatec/mapper/PMQueryProvider.java
@@ -76,25 +76,4 @@ public class PMQueryProvider {
         return sb.toString();
     }
 
-    public String selectSaleListALL(final UserVO userVO)
-    {
-        StringBuffer sb = new StringBuffer();
-        sb.append("SELECT count(*) FROM (SELECT count(*) FROM T_USER U LEFT JOIN T_USER_ROLE UR ON U.id = UR.userId");
-        sb.append(" LEFT JOIN (SELECT A.id,B.name as referrer FROM T_USER AS  A INNER JOIN T_USER as B ON A.referrer = B.id) as DD ON DD.id = U.id WHERE 1=1 ");
-        String[] roleids = userVO.getParaRoleIDS().split(",");
-
-        if (roleids != null){
-            for (int i = 0 ; i< roleids.length ; i ++){
-                String s = roleids[i];
-                if(i != roleids.length -1){
-                    sb.append("'" + s + "'" + ",");
-                }else{
-                    sb.append("'" + s + "'");
-                }
-            }
-        }
-        sb.append(" and U.state = 1 GROUP BY U.id ORDER BY modifyTime DESC");
-        sb.append(") as A");
-        return sb.toString();
-    }
 }

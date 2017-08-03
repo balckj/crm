7f0c9b9e41a3e4dbc55f7e3d59e80eafc2075455
qswdl <414946742@qq.com>
2017-08-03 14:20:16 +0800

    55

--- src/main/java/com/yidatec/mapper/CustomerMapper.java
+++ src/main/java/com/yidatec/mapper/CustomerMapper.java
@@ -29,11 +29,11 @@ public interface CustomerMapper {
     @SelectProvider(type=com.yidatec.mapper.CustomerQueryProvider.class,method = "countCustomer")
     int countCustomerList(CustomerVO dictionaryVO);
 
-    @Insert("INSERT INTO T_CUSTOMER (id,companyName,companyId,industry,nature,country,province,city,region,address,level,state,creatorId,createTime,modifierId,modifyTime) VALUES (#{id},#{companyName},#{companyId}," +
+    @Insert("INSERT INTO T_CUSTOMER (id,name,industry,nature,country,province,city,region,address,level,state,creatorId,createTime,modifierId,modifyTime) VALUES (#{id},#{name}," +
             "#{industry},#{nature},#{country},#{province},#{city},#{region},#{address},#{level},#{state},#{creatorId},#{createTime},#{modifierId},#{modifyTime})")
     int create(Customer customer);
 
-    @Update("UPDATE T_CUSTOMER SET `companyName`=#{companyName},companyId=#{companyId},industry=#{industry},nature=#{nature}," +
+    @Update("UPDATE T_CUSTOMER SET `name`=#{name},industry=#{industry},nature=#{nature}," +
             "country=#{country},province=#{province},city=#{city},region=#{region},address=#{address},level=#{level},region=#{region},state=#{state}," +
             "modifierId=#{modifierId}," +
             "modifyTime=#{modifyTime} WHERE id=#{id}")

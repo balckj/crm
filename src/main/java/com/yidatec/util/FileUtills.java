d53edfe3cdb66779c4bfd2b5d9cfa7bddccacd9c
dafei <344739106@qq.com>
2017-08-08 08:56:47 +0800

    update

---
+++ src/main/java/com/yidatec/util/FileUtills.java
@@ -0,0 +1,47 @@
+package com.yidatec.util;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/8/4.
+ */
+public class FileUtills {
+
+    /*
+    文件上传
+     */
+    public static Map FileUpload(MultipartFile file, String fileUrl) throws IOException {
+        String serverName = new Date().getTime()+"_"+file.getOriginalFilename();
+        String path=ConfigProperties.getFileUrl(fileUrl)+serverName;
+        File newFile=new File(path);
+        //通过CommonsMultipartFile的方法直接写文件（注意这个时候）
+        file.transferTo(newFile);
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("url",serverName);
+        return map;
+    }
+
+    /**
+     * 文件删除
+     * @return
+     * @throws Exception
+     */
+    public static String filedelete(String filename)
+            throws Exception {
+        String result="";
+        File targetFile = new File(filename);
+        if (targetFile.exists()) {
+            targetFile.delete();
+            result="success";
+        } else {
+            result="failed";
+        }
+        return result;
+    }
+}

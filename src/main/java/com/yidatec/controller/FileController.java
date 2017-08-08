0c9e4932218770c22bc2bc7cedcdcc75a028b771
dafei <344739106@qq.com>
2017-08-08 08:55:28 +0800

    update

--- src/main/java/com/yidatec/controller/FileController.java
+++ src/main/java/com/yidatec/controller/FileController.java
@@ -1,6 +1,6 @@
 package com.yidatec.controller;
 
-import com.yidatec.util.ConfigProperties;
+import com.yidatec.util.FileUtills;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,9 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
 import java.io.IOException;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,17 +26,7 @@ public class FileController extends BaseController{
     public Map handleFileUpload(@RequestParam("files") MultipartFile file) throws IOException {
         long  startTime=System.currentTimeMillis();
         System.out.println("fileName："+file.getOriginalFilename());
-        String serverName = new Date().getTime()+file.getOriginalFilename();
-        String path=ConfigProperties.getSetting("path")+serverName;
-        File newFile=new File(path);
-
-        //通过CommonsMultipartFile的方法直接写文件（注意这个时候）
-        file.transferTo(newFile);
-        long  endTime=System.currentTimeMillis();
-        System.out.println("方法二的运行时间："+String.valueOf(endTime-startTime)+"ms");
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put("url",serverName);
-        return map;
+        return FileUtills.FileUpload(file,"path");
     }
 
     /**
@@ -52,22 +40,18 @@ public class FileController extends BaseController{
     public Map filedelete(@RequestParam("name") String name, HttpServletResponse response, HttpServletRequest request)
             throws Exception {
         Map<String, Object> reMap = new HashMap<String, Object>();
-        // 文件路径
-//        String path = "E:/";
-//        String newFileName = name ;
-        File targetFile = new File(name);
-        if (targetFile.exists()) {
-            targetFile.delete();
-            reMap.put("code",200);
-//            Boolean bool = exhibitionpoService.deleteFile(id);
+        String result =FileUtills.filedelete(name);
+        if(result == "success"){
+            //删除数据库记录
+//            Boolean bool = fileService.deleteFile(id);
 //            if (bool == true) {
 //                reMap.put("mess", "附件删除成功！");
 //            } else {
 //                reMap.put("mess", "附件删除失败！");
 //            }
-        } else {
-//            reMap.put("mess", "附件不存在！");
-
+            reMap.put("code",200);
+        }else {
+            reMap.put("code",400);
         }
         return reMap;
     }

1645cd0b0b4553c8338d46ff8d1274c3f2c06121
qswdl <414946742@qq.com>
2017-08-09 10:40:32 +0800

    55

--- src/main/java/com/yidatec/controller/WechatCustomerController.java
+++ src/main/java/com/yidatec/controller/WechatCustomerController.java
@@ -24,7 +24,7 @@ import java.util.UUID;
  * Created by Qu on 2017/8/9.
  */
 @CrossOrigin(origins = "*")
-@RestController("/mobile")
+@RestController
 public class WechatCustomerController extends BaseController{
 
     @Autowired
@@ -35,17 +35,18 @@ public class WechatCustomerController extends BaseController{
 
 
 
-    @RequestMapping("/customerEdit")
-    public String customerEdit(ModelMap model,@RequestParam(value="id",required = false) String id){
-        model.put("title",(id == null || id.isEmpty())?"新建客户":"编辑客户");
-        model.put("customer",customerService.selectCustomer(id));
-        model.put("industryList",dictionaryService.selectDictionaryListByCodeCommon(Constants.GOOD_AT_INDUSTRY_CODE));// 所属行业
-        model.put("natureList",dictionaryService.selectDictionaryListByCodeCommon(Constants.NATURE_CODE));// 企业性质
-        model.put("levelList",dictionaryService.selectDictionaryListByCodeCommon(Constants.PLATFORM_LEVEL));// 平台等级
-        return "customerEdit";
+    @RequestMapping(value="/customer/{customerId}",method=RequestMethod.GET)
+    public Map<String,Object> customerEdit(ModelMap model,@RequestParam(value="id",required = false) String id){
+//        model.put("title",(id == null || id.isEmpty())?"新建客户":"编辑客户");
+//        model.put("customer",customerService.selectCustomer(id));
+//        model.put("industryList",dictionaryService.selectDictionaryListByCodeCommon(Constants.GOOD_AT_INDUSTRY_CODE));// 所属行业
+//        model.put("natureList",dictionaryService.selectDictionaryListByCodeCommon(Constants.NATURE_CODE));// 企业性质
+//        model.put("levelList",dictionaryService.selectDictionaryListByCodeCommon(Constants.PLATFORM_LEVEL));// 平台等级
+//        return "customerEdit";
+        return null;
     }
 
-    @RequestMapping(value = "/findCustomer")
+    @RequestMapping(value = "/customers",method=RequestMethod.POST)
     public Object findCustomer(@RequestBody CustomerVO customerVO)throws Exception{
         List<Customer> CustomerEntityList = customerService.selectCustomerList(customerVO);
         if (CustomerEntityList!=null){
@@ -71,38 +72,38 @@ public class WechatCustomerController extends BaseController{
     }
 
 
-    @RequestMapping("/saveCustomer")
-    @ResponseBody
-    public Object saveCustomer(@Validated @RequestBody Customer customer,
-                                 BindingResult result)throws Exception{
-        List<FieldError> errors = result.getFieldErrors();
-        if(errors  != null && errors.size() > 0){
-            return errors;
-        }
-        CustomerVO customer1 = new CustomerVO();
-//        Contact contact = new Contact();
-        if(customer.getId() == null || customer.getId().trim().length() <= 0)//新建
-        {
-            customer.setId(UUID.randomUUID().toString());
-            customer.setCreatorId(getWebUser().getId());
-            customer.setCreateTime(LocalDateTime.now());
-            customer.setModifierId(getWebUser().getCreatorId());
-            customer.setModifyTime(customer.getCreateTime());
-            customerService.createCustomer(customer);
-
-        } else {//编辑
-            customer.setModifierId(getWebUser().getId());
-            customer.setModifyTime(LocalDateTime.now());
-            customerService.updateCustomer(customer);
-        }
-        return getSuccessJson(null);
-    }
-
-    @RequestMapping("/getContact")
-    @ResponseBody
-    public Object getContact(@Validated @RequestBody String id){
-        customerService.getContact(id);
-        return getSuccessJson(null);
-    }
+//    @RequestMapping("/saveCustomer")
+//    @ResponseBody
+//    public Object saveCustomer(@Validated @RequestBody Customer customer,
+//                                 BindingResult result)throws Exception{
+//        List<FieldError> errors = result.getFieldErrors();
+//        if(errors  != null && errors.size() > 0){
+//            return errors;
+//        }
+//        CustomerVO customer1 = new CustomerVO();
+////        Contact contact = new Contact();
+//        if(customer.getId() == null || customer.getId().trim().length() <= 0)//新建
+//        {
+//            customer.setId(UUID.randomUUID().toString());
+//            customer.setCreatorId(getWebUser().getId());
+//            customer.setCreateTime(LocalDateTime.now());
+//            customer.setModifierId(getWebUser().getCreatorId());
+//            customer.setModifyTime(customer.getCreateTime());
+//            customerService.createCustomer(customer);
+//
+//        } else {//编辑
+//            customer.setModifierId(getWebUser().getId());
+//            customer.setModifyTime(LocalDateTime.now());
+//            customerService.updateCustomer(customer);
+//        }
+//        return getSuccessJson(null);
+//    }
+//
+//    @RequestMapping("/getContact")
+//    @ResponseBody
+//    public Object getContact(@Validated @RequestBody String id){
+//        customerService.getContact(id);
+//        return getSuccessJson(null);
+//    }
 
 }

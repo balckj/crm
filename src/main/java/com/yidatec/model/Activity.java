5c07de71589013d032da795becd06697ef76200e
qswdl <414946742@qq.com>
2017-08-11 15:02:10 +0800

    55

--- src/main/java/com/yidatec/model/Activity.java
+++ src/main/java/com/yidatec/model/Activity.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.yidatec.util.CustomLocalDateTime2Serializer;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -17,11 +19,13 @@ public class Activity extends BaseModel{
     @NotBlank(message = "必须输入活动名称", groups = { })
     @Length( max = 200, message = "名称最多由200个字符组成", groups = { })
     private String name;
-    @NotBlank(message = "必须输入开始时间", groups = { })
+    @NotNull(message = "必须输入开始时间", groups = { })
     @JsonSerialize(using = CustomLocalDateTime2Serializer.class)
+//    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime startDate;
-    @NotBlank(message = "必须输入结束时间", groups = { })
+    @NotNull(message = "必须输入结束时间", groups = { })
     @JsonSerialize(using = CustomLocalDateTime2Serializer.class)
+//    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime endDate;
     @NotBlank(message = "必须选择国家", groups = { })
     private String country;

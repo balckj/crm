af08e2a38b490a160e22388e31120865bc302302
HELLO WORLD <308864211@qq.com>
2017-09-27 08:51:00 +0800

    Merge branch 'master' of E:\WorkSpace\crm with conflicts.

--- src/main/java/com/yidatec/util/UUIDGenerator.java
+++ src/main/java/com/yidatec/util/UUIDGenerator.java
@@ -9,5 +9,11 @@ public class UUIDGenerator {
 
     public  static final void main(String[] args){
         System.out.println(UUID.randomUUID().toString());
+        System.out.println(UUID.randomUUID().toString());
+        System.out.println(UUID.randomUUID().toString());
+        System.out.println(UUID.randomUUID().toString());
+        System.out.println(UUID.randomUUID().toString());
+        System.out.println(UUID.randomUUID().toString());
+        System.out.println(UUID.randomUUID().toString());
     }
 }

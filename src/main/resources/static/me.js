2ce173be0ddd8718526870780692d119e20cd272
HELLO WORLD <308864211@qq.com>
2017-09-27 09:29:59 +0800

    toastr["success"]('保存成功!'); add

--- src/main/resources/static/me.js
+++ src/main/resources/static/me.js
@@ -109,3 +109,19 @@ function v(id,param,url){
         }
     });
 }
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "progressBar": true,
+    "preventDuplicates": false,
+    "positionClass": "toast-top-center",
+    "onclick": null,
+    "showDuration": "400",
+    "hideDuration": "1000",
+    "timeOut": "7000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
